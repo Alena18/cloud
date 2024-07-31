cf6cb0d4ef412e51daf5cff6e826616eb33e058b
Elena <coffeebeanstudio1809@gmail.com>
2024-07-31 15:16:25 +0100

    Reduce amout off tech balls appears on mobile screens

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
   const [buttonVisible, setButtonVisible] = useState(true);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); // Adjust width as needed
 
   const handleBallTouch = () => {
     setButtonVisible(false);
   };
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768); // Adjust width as needed
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // Determine number of balls to display based on screen size
+  const visibleTechnologies = isMobile
+    ? technologies.slice(0, 4)
+    : technologies;
 
   return (
     <div className="flex flex-col items-center">
@@ -21,7 +34,7 @@ const Tech = () => {
         </button>
       )}
       <div className="flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
+        {visibleTechnologies.map((technology) => (
           <div className="w-28 h-28" key={technology.name}>
             <BallCanvas icon={technology.icon} onBallTouch={handleBallTouch} />
           </div>
